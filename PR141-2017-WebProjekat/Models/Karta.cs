26584e88175353e418d178d1103e1f262a5b0997
Luka Tomic <tomicluka24@gmail.com>
2020-09-14 16:45:14 +0200

    Karta.cs

---
+++ PR141-2017-WebProjekat/Models/Karta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PR141_2017_WebProjekat.Models
+{
+    public class Karta
+    {
+        public string ID { get; set; }
+        public Manifestacija Manifestacija { get; set; }
+        public DateTime DatumIVremeManifestacije { get; set; }
+        public double Cena { get; set; }
+        public string Kupac { get; set; }
+        public bool isRezervisana { get; set; }
+        public string Tip { get; set; }
+
+    }
+}
