d6e97fe0145089de42b24e90166f6a5ad8c33b30
Luka Tomic <tomicluka24@gmail.com>
2020-09-14 16:38:02 +0200

    MestoOdrzavanja.cs

---
+++ PR141-2017-WebProjekat/Models/MestoOdrzavanja.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PR141_2017_WebProjekat.Models
+{
+    public class MestoOdrzavanja
+    {
+        public string Mesto { get; set; }
+        public double PostanskiBroj { get; set; }
+        public string Ulica { get; set; }
+        public int Broj { get; set; }
+    }
+}
