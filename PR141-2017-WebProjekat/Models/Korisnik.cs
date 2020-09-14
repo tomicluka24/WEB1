b33dc776c5741e8403c3d91def6dac551cd7c5a0
Luka Tomic <tomicluka24@gmail.com>
2020-09-14 15:54:06 +0200

    Inicijalni komit // Korisnik.cs

---
+++ PR141-2017-WebProjekat/Models/Korisnik.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PR141_2017_WebProjekat.Models
+{
+    public class Korisnik
+    {
+        public string KorisnickoIme { get; set; } //jedinstveno
+        public double Lozinka { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Pol { get; set; }
+        public DateTime DatumRodjenja { get; set; }
+        public string Uloga { get; set; }
+        //public Dictionary<string, Karta> SveKarteBezObziraNaStatus { get; set; } //ako je korisnik kupac
+        //public List<Manifestacija> Manifestacije { get; set; } //ako je korisnik prodavac
+        public double BrojSakupljenihBodova { get; set; } //ako je korisnik prodavac
+        public string TipKorisnika { get; set; } // koja je razlika izmedju uloge i tipa?
+
+    }
+}
