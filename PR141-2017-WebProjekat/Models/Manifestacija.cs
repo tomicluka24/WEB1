ffc9a3ea7fe0be13e98646d6b4e6bae33271a3b1
Luka Tomic <tomicluka24@gmail.com>
2020-09-14 16:41:56 +0200

    Manifestacija.cs

---
+++ PR141-2017-WebProjekat/Models/Manifestacija.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PR141_2017_WebProjekat.Models
+{
+    public class Manifestacija
+    {
+        public string Naziv { get; set; }
+        public string TipManifestacije { get; set; }
+        public int BrojMesta { get; set; }
+        public DateTime DatumIVremeOdrzavanja { get; set; }
+        public double CenaRegularneKarte { get; set; }
+        public bool IsAktivno { get; set; }
+        public MestoOdrzavanja MestoOdrzavanja { get; set; }
+        //dodaj za sliku
+    }
+}
