b0c0112ab9a54e7507e552436aa0cebc006c14bf
Luka Tomic <tomicluka24@gmail.com>
2020-09-14 16:22:22 +0200

    Lokacija

---
+++ PR141-2017-WebProjekat/Models/Lokacija.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PR141_2017_WebProjekat.Models
+{
+    public class Lokacija
+    {
+        public double GeografskaDuzina { get; set; }
+        public double GeografskaSirina { get; set; }
+        public MestoOdrzavanja MestoOdrzavanja { get; set; }
+
+    }
+}
