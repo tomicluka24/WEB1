c24e3dbf41729b5e31e7c8181d1336197e4658a3
Luka Tomic <tomicluka24@gmail.com>
2020-09-15 12:09:13 +0200

    zavrseno prijavljivanje sa prebacivanjem na odredjeni view u odnosu na ulogu korisnika

--- PR141-2017-WebProjekat/Controllers/AutentifikacijaController.cs
+++ PR141-2017-WebProjekat/Controllers/AutentifikacijaController.cs
@@ -10,9 +10,26 @@ namespace PR141_2017_WebProjekat.Controllers
     public class AutentifikacijaController : Controller
     {
         //GET: Autentifikacija
-        public ActionResult Index()
+       // [HttpPost]
+        public ActionResult Prijavljivanje(string korisnickoIme, string lozinka)
         {
-            return View();
+            List<Korisnik> korisnici = (List<Korisnik>)HttpContext.Application["korisnici"];
+            Korisnik korisnik = korisnici.Find(u => u.KorisnickoIme.Equals(korisnickoIme) && u.Lozinka.Equals(lozinka));
+
+            if (korisnik == null)
+            {
+                return View("Prijavljivanje");
+            }
+
+            Session["korisnik"] = korisnik;
+
+            if (korisnik.Uloga == "administrator")
+                return RedirectToAction("Index", "Administrator");
+            else if (korisnik.Uloga == "prodavac")
+                return RedirectToAction("Index", "Prodavac");
+            else 
+                return RedirectToAction("Index", "Kupac");
+
         }
 
         public ActionResult Registracija()
@@ -33,26 +50,5 @@ namespace PR141_2017_WebProjekat.Controllers
             return RedirectToAction("Prijavljivanje", "Autentifikacija"); //return to login when registration is completed
         }
 
-        [HttpPost]
-        public ActionResult Prijavljivanje(string korisnickoIme, string lozinka)
-        {
-            List<Korisnik> korisnici = (List<Korisnik>)HttpContext.Application["korisnici"];
-            Korisnik korisnik = korisnici.Find(u => u.KorisnickoIme.Equals(korisnickoIme) && u.Lozinka.Equals(lozinka));
-
-            if (korisnik == null)
-            {
-                ViewBag.Message = $"Korisnik ne postoji!";
-                return View("Index");
-            }
-
-            Session["korisnik"] = korisnik;
-
-            if (korisnik.Uloga == "administrator")
-                return RedirectToAction("Index", "Administrator");
-            else if (korisnik.Uloga == "prodavac")
-                return RedirectToAction("Index", "Prodavac");
-            else
-                return RedirectToAction("Index", "Kupac");
-        }
     }
 }
